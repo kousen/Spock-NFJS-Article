868843eb5288a63e34f01eed157e668f063bd6f2
Kenneth Kousen <ken.kousen@kousenit.com>
2012-02-20 21:47:37 -0600

    switched test to use @FailsWith

--- src/test/groovy/mjg/StringSpec.groovy
+++ src/test/groovy/mjg/StringSpec.groovy
@@ -1,47 +1,42 @@
 package mjg
 
+import spock.lang.FailsWith;
 import spock.lang.Specification;
 
 class StringSpec extends Specification {
-    String s
-    
-    def setup() {
-        s = "Live Long and Prosper"
-    }
+    String llap = "Live Long and Prosper"
     
     def "LLaP has 21 characters"() {
-        expect: s.size() == 21
+        expect: llap.size() == 21
     }
     
     def "LLaP has 4 words"() {
-        expect: s.split(/\W/).size() == 4
+        expect: llap.split(/\W/).size() == 4
     }
     
     def "LLaP has 6 vowels"() {
         expect:
-        s.findAll(/[aeiou]/).size() == 6
+        llap.findAll(/[aeiou]/).size() == 6
     }
 	
     def "Access inside the string doesn't throw an exception"() {
         when:
-        s[0..<s.size()]  // forward
-        s[-1..-s.size()] // backward
+        llap[0..<llap.size()]  // forward
+        llap[-1..-llap.size()] // backward
 
         then:
         notThrown(IndexOutOfBoundsException)
     }
 
+	@FailsWith(IndexOutOfBoundsException)
     def "Access before the start of the string throws exception"() {
-        when:
-        s[-s.size() -1]
-        
-        then:
-        def e = thrown(IndexOutOfBoundsException)
+        expect:
+        llap[-llap.size() -1]        
     }
 
 	def "Access beyond the end of the string throws exception"() {
 		when:
-        s[s.size()]
+        llap[llap.size()]
         
 		then:
 		IndexOutOfBoundsException e = thrown()
